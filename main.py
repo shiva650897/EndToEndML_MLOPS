4f5dbcf1e6cf47d86ea544bb7992ee740e6fd69b
shiva650897 <sk650897@gmail.com>
2024-07-24 14:43:56 +0530

    model trainer added

--- main.py
+++ main.py
@@ -2,18 +2,20 @@ from MLproject import logger
 from MLproject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from MLproject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from MLproject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from MLproject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 
 
 
-STAGE_NAME = "Data Ingestion Stage"
-try:
-    logger.info(f"***** stage {STAGE_NAME} started *******")
-    data_ingestion = DataIngestionTrainingPipeline()
-    data_ingestion.main()
-    logger.info(f"***** stage {STAGE_NAME} completed *******")
-except Exception as e:
-    logger.exception(e)
-    raise e
+
+# STAGE_NAME = "Data Ingestion Stage"
+# try:
+#     logger.info(f"***** stage {STAGE_NAME} started *******")
+#     data_ingestion = DataIngestionTrainingPipeline()
+#     data_ingestion.main()
+#     logger.info(f"***** stage {STAGE_NAME} completed *******")
+# except Exception as e:
+#     logger.exception(e)
+#     raise e
 
 STAGE_NAME = "Data Validation Stage"
 try:
@@ -25,11 +27,21 @@ except Exception as e:
     logger.exception(e)
     raise e
 
-STAGE_NAME = "Data Transformation Stage"
+# STAGE_NAME = "Data Transformation Stage"
+# try:
+#     logger.info(f"***** stage {STAGE_NAME} started *******")
+#     data_transformation = DataTransformationTrainingPipeline()
+#     data_transformation.main()
+#     logger.info(f"***** stage {STAGE_NAME} completed *******")
+# except Exception as e:
+#     logger.exception(e)
+#     raise e
+
+STAGE_NAME = "Model Trainer Stage"
 try:
     logger.info(f"***** stage {STAGE_NAME} started *******")
-    data_transformation = DataTransformationTrainingPipeline()
-    data_transformation.main()
+    model_trainer = ModelTrainerTrainingPipeline()
+    model_trainer.main()
     logger.info(f"***** stage {STAGE_NAME} completed *******")
 except Exception as e:
     logger.exception(e)
