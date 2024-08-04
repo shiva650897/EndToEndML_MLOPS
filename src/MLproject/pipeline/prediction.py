e52d1e2fdf5645ff79f09d9e442fac17cfea73c4
shiva650897 <sk650897@gmail.com>
2024-08-04 17:34:08 +0530

    ci cd added

---
+++ src/MLproject/pipeline/prediction.py
@@ -0,0 +1,14 @@
+import numpy as np
+import joblib
+import pandas as pd
+from pathlib import Path
+
+class PredictionPipeline:
+    def __init__(self) -> None:
+        self.model = joblib.load(Path('artifacts/model_trainer/model.joblib'))
+
+    
+    def predict(self,data):
+        prediction = self.model.predict(data)
+
+        return prediction
